b0f705694af8c3d8842abf8f295037d0f514e87e
DESKTOP-CTB0QFB\Filote Cosmin <filote_cosmin96@yahoo.com>
2018-03-02 14:57:39 +0200

    Billing in-app Example

---
+++ Metrorex/app/src/main/java/com/filote/cosmin/metrorex/Activities/StartGIf.java
@@ -0,0 +1,35 @@
+package com.filote.cosmin.metrorex.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.filote.cosmin.metrorex.R;
+
+import pl.droidsonroids.gif.GifDrawable;
+
+public class StartGIf extends AppCompatActivity {
+
+    GifDrawable gifDrawable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_gif);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //aici animatie sau ceva
+
+                startActivity(new Intent(StartGIf.this, Register.class));
+                finish();
+
+            }
+        },3000);
+
+    }
+
+
+}
